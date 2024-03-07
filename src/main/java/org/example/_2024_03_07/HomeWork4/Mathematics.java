9174b7cc3a5ff911892b93386f792acc7f3f6588
DegtiarovaInna <innade94@gmail.com>
2024-03-07 17:32:05 +0200

    1

---
+++ src/main/java/org/example/_2024_03_07/HomeWork4/Mathematics.java
@@ -0,0 +1,62 @@
+package org.example._2024_03_07.HomeWork4;
+
+import java.util.Scanner;
+
+//Реализовать программу, выводящую на экран результаты:
+//Сложения двух чисел
+//Вычитания двух чисел
+//Умножения двух чисел
+//Деления двух чисел
+//Каждая из арифметических операций должна быть реализована как отдельный метод.
+public class Mathematics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число: ");
+        int numb1 = scanner.nextInt();
+
+        System.out.println("Введите второе число: ");
+        int numb2 = scanner.nextInt();
+
+       int add =addition(numb1, numb2);
+        System.out.println("Результат сложения: " + add);
+
+        int sub =subtraction(numb1, numb2);
+        System.out.println("Результат вычитания: " + sub);
+
+        int mult =multiplication(numb1, numb2);
+        System.out.println("Результат умножения: " + mult);
+
+        double div =division(numb1, numb2);
+        System.out.println("Результат деления: " + div);
+    }
+
+    public static int addition(int numb1, int numb2){
+        int add = numb1+numb2;
+        return add;
+    }
+
+    public static int subtraction(int numb1, int numb2){
+        int sub = numb1-numb2;
+        return sub;
+    }
+
+    public static int multiplication(int numb1, int numb2){
+        int mult = numb1*numb2;
+        return mult;
+    }
+
+    public static double division(double numb1, double numb2){
+        double div =1;
+       if (numb2!=0){
+           div = numb1/numb2;
+           return div;
+       } else{
+           System.out.println("На ноль делить нельзя");
+           return 0;
+       }
+
+
+    }
+
+}
---
+++ src/main/java/org/example/_2024_03_07/HomeWork4/Words.java
@@ -0,0 +1,29 @@
+package org.example._2024_03_07.HomeWork4;
+
+import java.util.Scanner;
+
+//Введите 2 слова, воспользуйтесь сканером, состоящие из четного количества букв (проверьте количество букв в слове).
+//Нужно получить слово, состоящее из первой половины первого слова и второй половины второго слова. распечатать на консоль.
+//Например:
+//ввод - mama, papa
+//вывод - mapa
+public class Words {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое слово с четным количеством букв: ");
+        String wordOne = scanner.next();
+
+        System.out.println("Введите второе словос четным количеством букв: ");
+        String wordTwo = scanner.next();
+
+
+        if (wordOne.length()%2==0 && wordTwo.length()%2==0) {
+            String wordOnehalf = wordOne.substring(wordOne.length()/2); // .substring нашла в интернете, как сделать без?)
+            String wordTwoHalf = wordTwo.substring(wordTwo.length()/2);
+            String result = wordOnehalf+wordTwoHalf;
+            System.out.println("Вот ответ: "+ result);
+        } else {
+            System.out.println("Вы ввели неверной длины слово");
+        }
+    }
+}
